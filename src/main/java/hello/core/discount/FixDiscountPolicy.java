81d8a65e88e37a08860e427caf4ca09fa7ae83b6
Heyna-log <rhskgus048@naver.com>
2023-04-03 15:54:37 +0900

    주문과 할인 도메인 개발

---
+++ src/main/java/hello/core/discount/FixDiscountPolicy.java
@@ -0,0 +1,21 @@
+package hello.core.discount;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+
+public class FixDiscountPolicy implements DiscountPolicy {
+	
+	private int discountFixAmount = 1000; // 1000원 할인
+
+	@Override
+	public int discount(Member member, int price) {
+		
+		// 회원 등급이 VIP이면 할인금액이 1000원, 아니면 0원
+		if (member.getGrade() == Grade.VIP) {
+			return discountFixAmount;
+		} else {			
+			return 0;
+		}
+	}
+
+}
