cc75c366a2be54a523ca4e173767fe84b243e7f9
Heyna-log <rhskgus048@naver.com>
2023-04-04 14:37:09 +0900

    AppConfig 리팩토링(역할에 따른 구현 클래스 정리)

--- project/src/main/java/hello/core/AppConfig.java
+++ project/src/main/java/hello/core/AppConfig.java
@@ -1,6 +1,8 @@
 package hello.core;
 
+import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
@@ -15,12 +17,28 @@ import hello.core.order.OrderServiceImpl;
  */
 
 public class AppConfig {
-
-	public MemberService memberService() {
-		return new MemberServiceImpl(new MemoryMemberRepository());
+	
+	// ** 회원 저장소
+	// memberRepository 구현 객체는 MemoryMemberRepository 사용
+	public MemberRepository memberRepository() {
+		return new MemoryMemberRepository();
 	}
 	
+	// ** 할인 정책
+	// discountPolicy 구현 객체는 FixDiscountPolicy 사용 (정액 할인 정책 사용)
+	public DiscountPolicy discountPolicy() {
+		return new FixDiscountPolicy();
+	}
+
+	// ** 회원 관련 Service
+	// memberService 구현 객체인 MemberServiceImpl에 memberRepository 구현 객체 주입
+	public MemberService memberService() {
+		return new MemberServiceImpl(memberRepository());
+	}
+	
+	// ** 주문 관련 Service
+	// orderService 구현 객체인 OrderServiceImpl에 memberRepository, discountPolicy 구현 객체 주입
 	public OrderService orderService() {
-		return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+		return new OrderServiceImpl(memberRepository(), discountPolicy());
 	}
 }
