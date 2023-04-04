7c8b1e6c11859ee5e289412830ad458b2b59ba9d
Heyna-log <rhskgus048@naver.com>
2023-04-04 10:33:40 +0900

    새로운 할인 정책 적용 / 문제점(DIP, OCP 위반) 관련 주석 추가

--- project/src/main/java/hello/core/order/OrderServiceImpl.java
+++ project/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -2,6 +2,7 @@ package hello.core.order;
 
 import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
@@ -9,7 +10,21 @@ import hello.core.member.MemoryMemberRepository;
 public class OrderServiceImpl implements OrderService {
 	
 	private final MemberRepository memberRepository = new MemoryMemberRepository();
-	private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+	
+	// 할인 정책
+//	private final DiscountPolicy discountPolicy = new FixDiscountPolicy(); // 정액 할인 정책
+	private final DiscountPolicy discountPolicy = new RateDiscountPolicy(); // 정를 할인 정책
+	
+	/*
+	 * ** DIP 위반
+	 *  - 추상(인터페이스) 의존 => DiscountPolicy
+	 *  - 구체(구현) 클래스 의존 => FixDiscountPolicy, RateDiscountPolicy
+	 *  
+	 * ** OCP 위반
+	 *  - 기능 확장, 변경 시 클라이언트 코드(Service)에 영향을 줌 => 할인 정책 변경 시 ServiceImpl 코드를 변경해야 한다.
+	 *  
+	 */
+	
 	
 
 	@Override
