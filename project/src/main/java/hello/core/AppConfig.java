8aa87d0a2e81dee4d830db6ec3a5efba52020e6e
Heyna-log <rhskgus048@naver.com>
2023-04-05 14:07:00 +0900

    스프링으로 전환 - 스프링 빈 등록

--- project/src/main/java/hello/core/AppConfig.java
+++ project/src/main/java/hello/core/AppConfig.java
@@ -1,5 +1,8 @@
 package hello.core;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
 import hello.core.discount.DiscountPolicy;
 import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.MemberRepository;
@@ -9,35 +12,35 @@ import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
 
-
-/*
- * 애플리케이션의 실제 동작에 필요한 구현 객체를 생성
- * 생성한 객체 인스턴스의 참조(레퍼런스)를 생성자를 통해서 주입(연결)
- *  => 생성자 주입 (Constructor Injection)
- */
-
+// **어노테이션 기반 자바 설정 클래스
+@Configuration // '애플리케이션 설정(구성) 정보 담당'을 의미
 public class AppConfig {
 	
-	// ** 회원 저장소
-	// memberRepository 구현 객체는 MemoryMemberRepository 사용
-	public MemberRepository memberRepository() {
-		return new MemoryMemberRepository();
-	}
+	/*
+	 * **스프링 빈 등록
+	 *  - @Bean이 붙어있는 메소드를 호출 후 반환되는 객체를 스프링 빈으로 등록
+	 *  - 메소드 이름을 빈 이름으로 사용
+	 *  - 빈 이름을 직접 부여할 수도 있음. ex) @Bean(name="memberService2")
+	 *  	- 빈 이름은 항상 다른 이름을 부여해야 함.
+	 *  	- 같은 이름을 부여하면 다른 빈이 무시되거나, 기존 빈을 덮어버리는 등 설정에 따라 오류가 발생할 수 있음.
+	 */
 	
-	// ** 할인 정책
-	// discountPolicy 구현 객체는 RateDiscountPolicy 사용 (정률 할인 정책 사용)
+	@Bean // 메소드 호출 후 반환되는 객체를 스프링 빈으로 등록
+	public MemberRepository memberRepository() {
+		return new MemoryMemberRepository(); // 빈으로 등록
+	}
+
+	@Bean
 	public DiscountPolicy discountPolicy() {
 		return new RateDiscountPolicy();
 	}
 
-	// ** 회원 관련 Service
-	// memberService 구현 객체인 MemberServiceImpl에 memberRepository 구현 객체 주입
+	@Bean
 	public MemberService memberService() {
 		return new MemberServiceImpl(memberRepository());
 	}
-	
-	// ** 주문 관련 Service
-	// orderService 구현 객체인 OrderServiceImpl에 memberRepository, discountPolicy 구현 객체 주입
+
+	@Bean
 	public OrderService orderService() {
 		return new OrderServiceImpl(memberRepository(), discountPolicy());
 	}
