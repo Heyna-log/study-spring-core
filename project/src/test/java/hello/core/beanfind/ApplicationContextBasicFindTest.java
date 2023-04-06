ea7b803178a849d6bfa7bc3ee3118d076a196c1d
Heyna-log <rhskgus048@naver.com>
2023-04-06 17:52:41 +0900

    스프링 빈 조회

---
+++ project/src/test/java/hello/core/beanfind/ApplicationContextBasicFindTest.java
@@ -0,0 +1,56 @@
+package hello.core.beanfind;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import hello.core.AppConfig;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+
+public class ApplicationContextBasicFindTest {
+
+	AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+	
+	@Test
+	@DisplayName("빈 이름으로 조회")
+	void findBeanByName() {
+		MemberService memberService = ac.getBean("memberService", MemberService.class);
+		
+//		System.out.println("memberService : " + memberService);
+//		System.out.println("memberService.getClass() : " + memberService.getClass());
+		
+		// memberService가 MemberServiceImpl 타입으로 받을 수 있는지 확인
+		assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
+	}
+	
+	@Test
+	@DisplayName("빈 이름 없이 타입으로만 조회")
+	void findBeanByType() {
+		MemberService memberService = ac.getBean(MemberService.class);
+		assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
+	}
+	
+	@Test
+	@DisplayName("구체 타입으로 조회")
+	void findBeanByName2() {
+		MemberServiceImpl memberService = ac.getBean("memberService", MemberServiceImpl.class);
+		assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
+	}
+	
+	@Test
+	@DisplayName("빈 이름으로 조회 X")
+	void findBeanByNameX() {
+		
+		/* **예외(Exception) 검증
+		 * ac.getBean("xxx", MemberService.class)을 실행했을 때 발생하는 예외(Exception)가 NoSuchBeanDefinitionException 타입인지 확인
+		 * */
+		
+		// assertj 아니고 junit 사용
+		assertThrows(NoSuchBeanDefinitionException.class, () -> ac.getBean("xxx", MemberService.class));
+	}
+}
