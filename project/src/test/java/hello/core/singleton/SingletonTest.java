a0c3f7f4847409a4359a828b8be64298ed0a06b2
Heyna-log <rhskgus048@naver.com>
2023-04-11 17:05:53 +0900

    싱글톤 컨테이너(=스프링 컨테이너)

--- project/src/test/java/hello/core/singleton/SingletonTest.java
+++ project/src/test/java/hello/core/singleton/SingletonTest.java
@@ -4,6 +4,8 @@ import static org.assertj.core.api.Assertions.*;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import hello.core.AppConfig;
 import hello.core.member.MemberService;
@@ -54,5 +56,26 @@ public class SingletonTest {
 		
 		// singletonService1 == singletonService2
 		assertThat(singletonService1).isSameAs(singletonService2);
+	}
+	
+	@Test
+	@DisplayName("스프링 컨테이너와 싱글톤")
+	void singletonContainer() {
+		
+		/* 스프링의 기본 빈 등록 방식 -> 싱글톤 */
+
+		// 스프링 컨테이너 생성(싱글톤 컨테이너)
+		ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+		
+		// 조회
+		MemberService memberService1 = ac.getBean("memberService", MemberService.class); // 호출1
+		MemberService memberService2 = ac.getBean("memberService", MemberService.class); // 호출2
+		
+		// 조회할 때마다 같은 객체를 반환하는지(참조값이 같은지) 확인 => 객체를 싱글톤으로 관리
+		System.out.println("memberService1 : " + memberService1);
+		System.out.println("memberService2 : " + memberService2);
+		
+		// memberService1 == memberService2
+		assertThat(memberService1).isSameAs(memberService2);
 	}
 }
